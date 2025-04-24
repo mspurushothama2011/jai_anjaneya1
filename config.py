3a68954d654e7a94ef75b4dbff00e0ed53a92c33
mspurushothama2011 <example@example.com>
2025-04-24 11:15:51 +0530

    update

--- config.py
+++ config.py
@@ -18,9 +18,9 @@ class Config:
     MAIL_SERVER = os.getenv("MAIL_SERVER", "smtp-relay.brevo.com")
     MAIL_PORT = int(os.getenv("MAIL_PORT", 587))
     MAIL_USE_TLS = os.getenv("MAIL_USE_TLS", "True") == "True"
-    MAIL_USERNAME = os.getenv("MAIL_USERNAME", "87b185003@smtp-brevo.com")
-    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD", "f0JGmvxsnA3ECLDO")
-    MAIL_DEFAULT_SENDER = os.getenv("MAIL_DEFAULT_SENDER", "mspurushothama2011@gmail.com")
+    MAIL_USERNAME = os.getenv("MAIL_USERNAME", "8b4ca1001@smtp-brevo.com")
+    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD", "P1r4DvZdMGFhk8Kc")
+    MAIL_DEFAULT_SENDER = os.getenv("MAIL_DEFAULT_SENDER", "mspurushothama20@gmail.com")
 
     # Razorpay Credentials
     RAZORPAY_KEY_ID = os.getenv("RAZORPAY_KEY_ID", "rzp_test_6WWw11VMvM8MXw")
